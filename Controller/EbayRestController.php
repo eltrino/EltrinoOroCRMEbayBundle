48a5a038b518d37d6c46866644712b8e51c7b13b
Ruslan Voitenko <psw1986@gmail.com>
2014-04-14 14:58:50 +0300

    change namespace

--- Controller/EbayRestController.php
+++ Controller/EbayRestController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Eltrino\EbayBundle\Controller;
+namespace Eltrino\OroCrmEbayBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
